a636e0328f16f3fd2247593a024f276b58ec077d
Steve Kemp <steve@steve.org.uk>
2020-04-18 11:11:38 +0300

    Attempt to build the three images

---
+++ part2/ex2.7/build-images.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Build the three docker images.
+#
+
+
+start=$(pwd)
+
+# frontend
+git clone https://github.com/docker-hy/ml-kurkkumopo-frontend.git
+cd ml-kurkkumopo-frontend.git
+docker build -t ml-kurkkumopo-frontend .
+
+# backend
+cd ${start}
+git clone https://github.com/docker-hy/ml-kurkkumopo-backend.git
+cd ml-kurkkumopo-backend
+docker build -t ml-kurkkumopo-backend .
+
+
+# training
+cd ${start}
+git clone https://github.com/docker-hy/ml-kurkkumopo-training.git
+cd ml-kurkkumopo-training
+docker build -t ml-kurkkumopo-training .
